000acd7de86a525e67633eb26e995a466db97622
Isaque-Br <izakesz@gmail.com>
2024-02-02 19:33:14 -0300

    Criando um endpoint REST que responde a solicitações GET /HTTP na rota localhost:8080

---
+++ src/main/java/com/personal/project/PersonalProjectApplication.java
@@ -0,0 +1,23 @@
+package com.personal.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@SpringBootApplication
+@RestController
+public class PersonalProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalProjectApplication.class, args);
+	}
+
+	@GetMapping
+	public String hello() {						// Only Endpoint here
+		return ("Hello World");
+	}
+
+}
